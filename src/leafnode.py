379a5bcbdfbca99a7743dbf537fca686f951bfc9
Khalfan AlShamsi <khalfanalshamsi02@gmail.com>
2026-03-10 11:53:52 +0400

    removed test case from inside leafnode class & fixed bug in to_html method

--- src/leafnode.py
+++ src/leafnode.py
@@ -1,7 +1,7 @@
 from htmlnode import *
 
 class LeafNode(HTMLNode):
-    def __init__(self, tag = None, value, props = None):
+    def __init__(self, tag, value, props = None):
         super().__init__(tag, value, props)
 
     def to_html(self):
@@ -9,13 +9,9 @@ class LeafNode(HTMLNode):
             raise ValueError(f'No value for leaf node {self}')
         if self.tag == None:
             return self.value
-        elif self.props is not None:
+        elif not self.props:
             return f'<{self.tag} {self.props}>{self.value}</{self.tag}>'
         return f'<{self.tag}>{self.value}</{self.tag}>'
     
     def __repr__(self):
         return f"HTMLNode(tag={self.tag}, value={self.value}, props={self.props})"
-    
-    def test_leaf_to_html_p(self):
-        node = LeafNode("p", "Hello, world!")
-        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
