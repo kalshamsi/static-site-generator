a3021ddb7c676a4bd477c22b15ffd73e3297caf5
Khalfan AlShamsi <khalfanalshamsi02@gmail.com>
2026-03-10 11:51:33 +0400

    added leaf node test cases

---
+++ src/test_leafnode.py
@@ -0,0 +1,8 @@
+import unittest
+
+from leafnode import *
+
+class test_leafnode(unittest.TestCase):
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
