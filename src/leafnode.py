0b30e9be690063f373c818d49506353114430cf0
Khalfan AlShamsi <khalfanalshamsi02@gmail.com>
2026-03-10 11:56:09 +0400

    fixed bug finally in leaf node to_html method

--- src/leafnode.py
+++ src/leafnode.py
@@ -2,7 +2,7 @@ from htmlnode import *
 
 class LeafNode(HTMLNode):
     def __init__(self, tag, value, props = None):
-        super().__init__(tag, value, props)
+        super().__init__(tag, value, props = props)
 
     def to_html(self):
         if self.value == None:
@@ -10,8 +10,8 @@ class LeafNode(HTMLNode):
         if self.tag == None:
             return self.value
         elif not self.props:
-            return f'<{self.tag} {self.props}>{self.value}</{self.tag}>'
-        return f'<{self.tag}>{self.value}</{self.tag}>'
+            return f'<{self.tag}>{self.value}</{self.tag}>'
+        return f'<{self.tag} {self.props}>{self.value}</{self.tag}>'
     
     def __repr__(self):
         return f"HTMLNode(tag={self.tag}, value={self.value}, props={self.props})"
