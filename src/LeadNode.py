5401655b895303869e22e62739328fd783c65c08
Khalfan AlShamsi <khalfanalshamsi02@gmail.com>
2026-03-10 11:17:47 +0400

    added leafnode class implementation

---
+++ src/LeadNode.py
@@ -0,0 +1,21 @@
+from htmlnode import *
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag = None, value = None, props = None):
+        super().__init__(tag, value, props)
+
+    def to_html(self):
+        if self.value == None:
+            raise ValueError(f'No value for leaf node {self}')
+        if self.tag == None:
+            return self.value
+        elif self.props is not None:
+            return f'<{self.tag} {self.props}>{self.value}</{self.tag}>'
+        return f'<{self.tag}>{self.value}</{self.tag}>'
+    
+    def __repr__(self):
+        return f"HTMLNode(tag={self.tag}, value={self.value}, props={self.props})"
+    
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
