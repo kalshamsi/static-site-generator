5f1456f438784b0f0e73078897ed5ea3d3cf6b4f
Khalfan AlShamsi <khalfanalshamsi02@gmail.com>
2026-03-12 20:24:05 +0400

    added split nodes by delimiter method and a bunch of unit tests for it

---
+++ src/delimiter.py
@@ -0,0 +1,24 @@
+from textnode import *
+from htmlnode import *
+
+def split_nodes_delimiter(old_nodes: list, delimiter: str, text_type: TextType):
+    new_nodes = []
+    new_strings = []
+
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+        else:
+            new_strings = node.text.split(delimiter)
+    
+            if len(new_strings) % 2 == 0:
+                raise ValueError("Invalid syntax")
+        
+            for i, sentence in enumerate(new_strings):
+                if sentence != "":
+                    if i % 2 == 0:
+                        new_nodes.append(TextNode(sentence, TextType.TEXT))
+                    else:
+                        new_nodes.append(TextNode(sentence, text_type))
+
+    return new_nodes
